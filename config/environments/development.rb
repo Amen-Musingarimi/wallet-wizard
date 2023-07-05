74591b7541c8773790897b96039cff252d7b04a0
Amen-Musingarimi <takudzwamusinga@gmail.com>
2023-07-05 14:46:26 +0200

    Add tests

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,9 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
+
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
+  
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
